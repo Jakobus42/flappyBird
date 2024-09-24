d9b345a60437b9c632be393c86e1bea6da8b81eb
Jakob Sadjina <jakobsadjina@Mac.telekom.ip>
2024-09-24 19:16:29 +0200

    playing around

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,23 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-  auto window = sf::RenderWindow{{1920u, 1080u}, "FlappyBird"};
+  auto window = sf::RenderWindow{{1920u, 1080u}, "Flappyird"};
   window.setFramerateLimit(144);
+  sf::Texture texture;
 
+  if (!texture.loadFromFile("assets/sprites/yellowbird-upflap.png"))
+      return EXIT_FAILURE;
+  sf::Sprite sprite(texture);
+  sprite.setScale(4, 4);
+  sprite.setPosition(window.getSize().x / 2, window.getSize().y / 2);
   while (window.isOpen()) {
     for (auto event = sf::Event{}; window.pollEvent(event);) {
       if (event.type == sf::Event::Closed) {
         window.close();
       }
     }
-
     window.clear();
+    window.draw(sprite);
     window.display();
   }
 }
