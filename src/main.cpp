e69b026c7ebd214e8a41f81ab55a06c7d35e27c2
Jakob Sadjina <jakobsadjina@Mac.telekom.ip>
2024-09-24 19:52:39 +0200

    feat: bird movement

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,34 @@
 #include <SFML/Graphics.hpp>
 
+void moveBird(sf::Sprite& bird, sf::Keyboard::Key key) {
+  switch (key) {
+    case sf::Keyboard::W: {
+      bird.move(0, -100);
+    }
+    default: return ;
+  }
+}
+
 int main() {
-  auto window = sf::RenderWindow{{1920u, 1080u}, "Flappyird"};
+  auto window = sf::RenderWindow{{1920u, 1080u}, "Flappybird"};
   window.setFramerateLimit(144);
   sf::Texture texture;
 
   if (!texture.loadFromFile("assets/sprites/yellowbird-upflap.png"))
       return EXIT_FAILURE;
-  sf::Sprite sprite(texture);
-  sprite.setScale(4, 4);
-  sprite.setPosition(window.getSize().x / 2, window.getSize().y / 2);
+  sf::Sprite bird(texture);
+  bird.setScale(4, 4);
+  bird.setPosition(window.getSize().x / 2, window.getSize().y / 2);
   while (window.isOpen()) {
     for (auto event = sf::Event{}; window.pollEvent(event);) {
-      if (event.type == sf::Event::Closed) {
-        window.close();
+      if (((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape)) || event.type == sf::Event::Closed) {
+          window.close();
       }
+      moveBird(bird, event.key.code);
     }
-    window.clear();
-    window.draw(sprite);
+    bird.move(0, 2);
+    window.draw(bird);
     window.display();
+    window.clear();
   }
 }
